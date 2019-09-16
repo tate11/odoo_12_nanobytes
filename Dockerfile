74232ed916c27beec1f56804e5b6e014b6a99032
ilazar1 <ilazar@nanobytes.es>
2019-09-16 13:05:38 +0200

    [UPDATE] Update modules,new module and new build odoo 12

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM registry2.nanobytes.es/odoo_12_ee:20190525
+FROM registry2.nanobytes.es/odoo_12_ee:latest
 MAINTAINER Nanobytes Informática y Telecomunicaciones <soporte@nanobytes.es>
 
 COPY . /mnt/extra-addons/
---
+++ perso_static_rancher/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Rancher2 Statics',
+    'author': 'Nanobytes Informática y Telecomunicaciones S.L',
+    'description': """
+
+            Nanobytes - Rancher2 Statics
+
+            This module was made with the intention to save data and expose for rancher2-catalog
+
+            This module was made by Nanobytes Informática y Telecomunicaciones S.L,
+            for more information, contact us : https://nanobytes.es
+
+        """,
+    'summary': 'Rancher2 Statics',
+    'category': 'Data',
+    'version': '1.2.4',
+    'depends': ['base'],
+    'data': [],
+    'application': False,
+    'installable': True
+}
